31279889ad7f5604c8b63d822c3be18dd8ac60e2
GreJangre <dlatjdwn8824@naver.com>
2019-04-01 19:56:58 +0900

    Demo Flip and Match card game

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount: Int = 0
+    var emojiChoices: Array<String> = ["👻","🎃","🎃","👻"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.7086189389, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        flipCount += 1
+        flipCountLabel.text = "Flips: \(flipCount)"
+    }
 }
-
